6f52a7091635cbef4a0854b2a4013a76185e2032
Toby Shi <toby3030@qq.com>
2022-09-20 01:25:48 +0800

    fix: td query site change

--- td_query.py
+++ td_query.py
@@ -6,7 +6,7 @@ from authorization.sso_login import sso_login
 
 if __name__ == '__main__':
     cookies = td_login(sso_login())
-    request = requests.get('http' + '://10.212.28.38/index.php?module=stu&menu=stu&title=stuSunData', cookies=cookies)
+    request = requests.get('http://10.212.28.38/main.php?module=stu&title=stu_sun_score', cookies=cookies)
     reflect = {'1': '秋季学期', '2': '春季学期'}
     pattern = '<td>(\\d+-\\d+)</td>\\s*<td>(\\d+)</td>\\s*<td>(\\d+)</td>\\s*<td>-</td>'
     info = [{'学期': i[0] + ' ' + reflect[i[1]], 'TD次数': i[2]} for i in re.findall(pattern, request.text)]
